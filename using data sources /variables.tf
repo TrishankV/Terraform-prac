1ca08a97e0e24d2233902cf46be5778398e4ae67
Trishank <96646928+TrishankV@users.noreply.github.com>
2026-02-10 14:45:26 +0530

    Create variables.tf

---
+++ using data sources /variables.tf
@@ -0,0 +1,26 @@
+# variables.tf 
+# Now we are creataingg variables such that it parse as ana environment 
+
+
+variable "app_environment" {
+    type = map(
+        object (
+            vnname = string 
+            vncidr = string 
+            subnets = map(
+                object (
+                    cidr = string 
+                )
+            )
+            niname  = string 
+            publibipname = string
+            vmname = string 
+        )
+    ) 
+
+}
+
+variable "password" { 
+    type = string
+    sensetive = true 
+}
