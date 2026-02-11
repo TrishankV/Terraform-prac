6a35d44ea9be3e9463fd73653a8a7c38f0202652
Trishank <96646928+TrishankV@users.noreply.github.com>
2026-02-11 15:54:23 +0530

    Refactor NIC and Public IP resource definitions

--- Windows Web Server + Dynamic/nic.tf
+++ Windows Web Server + Dynamic/nic.tf
@@ -1,8 +1,7 @@
 # nic.tf
-# creating multiple nics 
-
 resource "azurerm_network_interface" "nics" { 
-    name = var.app_environment.prod.niname
+    for_each = var.app_environment.prod.subnets["websubnet01"].machines
+    name = each.value.niname
     resource_group_name = local.rgname 
     location = local.loc
 
@@ -10,12 +9,13 @@ resource "azurerm_network_interface" "nics" {
         name = "internal"
         subnet_id = azurerm_subnet.app_subnets["websubnet01"].id 
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id = azurerm_public_ip.ipp.id
+        public_ip_address_id = azurerm_public_ip.ipp[each.key].id
     }
 }
 
 resource "azurerm_public_ip" "ipp" {
-    name = var.app_environment.prod.publibipname
+    for_each = var.app_environment.prod.subnets["websubnet01"].machines
+    name = each.value.publibipname
     resource_group_name = local.rgname
     location = local.loc
     allocation_method = "Static"
